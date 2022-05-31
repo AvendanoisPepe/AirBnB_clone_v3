a36014678df665eebf97abd66789230ae13b2a53
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-05-31 18:17:11 -0500

    intento?

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,8 +6,10 @@ from models import storage
 from api.v1.views import app_views
 from models.state import State
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
