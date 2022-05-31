631ab7cf0eedfb5fe136595f37172269e83b721c
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-05-31 00:36:25 -0500

    matenm

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -16,12 +16,14 @@ from models.city import City
     )
 def todosC(state_id):
     """ Todos los objetos de la ciudad """
-    estado = storage.get(State, state_id)
-    if estado is None:
+    list_cities = []
+    state = storage.get(State, state_id)
+    if not state:
         abort(404)
-    return jsonify(
-        [city.to_dict() for city in storage.all(City).values()]
-        )
+    for city in state.cities:
+        list_cities.append(city.to_dict())
+
+    return jsonify(list_cities)
 
 
 @app_views.route('cities/<city_id>', methods=['GET'], strict_slashes=False)
@@ -49,23 +51,20 @@ def eliminarC(city_id):
 @app_views.route(
     "/states/<state_id>/cities", methods=["POST"], strict_slashes=False
     )
-def post_city(state_id):
-    """
-    Creates a City
-    """
+def crearC(state_id):
+    """ Crea un nuevo objeto ciudad """
     state = storage.get(State, state_id)
-    if not state:
+    if state is None:
         abort(404)
-    if not request.get_json():
-        abort(400, description="Not a JSON")
-    if 'name' not in request.get_json():
-        abort(400, description="Missing name")
-
-    data = request.get_json()
-    instance = City(**data)
-    instance.state_id = state.id
-    instance.save()
-    return make_response(jsonify(instance.to_dict()), 201)
+    datos = request.get_json()
+    if datos is None:
+        abort(400, "Not a JSON")
+    if "name" not in datos:
+        abort(400, "Missing name")
+    nuevo = City(**datos)
+    nuevo.state_id = state_id
+    nuevo.save()
+    return make_response(jsonify(nuevo.to_dict()), 201)
 
 
 @app_views.route("/cities/<city_id>", methods=["PUT"], strict_slashes=False)
