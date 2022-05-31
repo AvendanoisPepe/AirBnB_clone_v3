6fe2fae4863698e5a9c0f45f2157cdf502c89cfe
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-05-31 00:40:16 -0500

    matenm

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -16,14 +16,13 @@ from models.city import City
     )
 def todosC(state_id):
     """ Todos los objetos de la ciudad """
-    list_cities = []
-    state = storage.get(State, state_id)
-    if not state:
+    ciudades = []
+    estado = storage.get(State, state_id)
+    if estado is None:
         abort(404)
-    for city in state.cities:
-        list_cities.append(city.to_dict())
-
-    return jsonify(list_cities)
+    for ciudad in state.cities:
+        ciudades.append(ciudad.to_dict())
+    return jsonify(ciudades)
 
 
 @app_views.route('cities/<city_id>', methods=['GET'], strict_slashes=False)
